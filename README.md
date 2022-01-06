25adc93646e5141282fd30a2150259ed26fbe153
Komefumi <komefumi@protonmail.ch>
2022-01-07 00:58:31 +0530

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# My Page
-
-Currently a work in progress, but intended to become my blog and home page
--- src/blog-posts/The_Beginning__05_01_2022.tsx
+++
@@ -1,5 +0,0 @@
-/*
- * This is the first blog posts and signifies a new start
- * And here we make merry
- */
-export default function TheBeginning() {}
